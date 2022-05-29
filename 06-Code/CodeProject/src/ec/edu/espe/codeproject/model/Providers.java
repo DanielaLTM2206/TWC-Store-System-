a85cdae9fc9683340e4f6dc45b192a927185b146
Bolivar Josue Villavicencio Campoverde <96481242+JosueVilla7158@users.noreply.github.com>
2022-05-29 16:51:21 -0500

    CodeProject

---
+++ 06-Code/CodeProject/src/ec/edu/espe/codeproject/model/Providers.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.codeproject.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Josue Villavicencio, DCCO - ESPE,MyWayCode
+ */
+public class Providers {
+    int cellPhone;
+    String providerName;
+    ArrayList<ViveresPedro> viveresPedros;
+    
+    
+}
