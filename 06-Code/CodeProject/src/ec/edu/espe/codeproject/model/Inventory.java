c5ee45a20ff0f3d3339399784488977fc9e830f8
Martin Toapanta <mhtoapanta1@espe.edu.ec>
2022-05-29 22:30:57 -0500

    Inventory.class

---
+++ 06-Code/CodeProject/src/ec/edu/espe/codeproject/model/Inventory.java
@@ -0,0 +1,29 @@
+package ec.edu.espe.codeproject.model;
+/**
+ *
+ * @author Martin Toapanta, DCCO-ESPE, MyWayCode
+ */
+public class Inventory {
+ private String product;
+ private int code;
+ private String type;
+ private int amount;
+ private float price;
+ private boolean discount;
+ private String date;
+ private String status; 
+ 
+ public void addProduct(){
+ }
+ public void editProduct(){
+ }
+ public productList searchProduct(){
+ return new productList();
+ }
+ public void deleteProduct(){
+ }
+ ArrayList<ProductList> productList;
+ public productList(ProductList productList){ 
+ }
+ }
+ 
