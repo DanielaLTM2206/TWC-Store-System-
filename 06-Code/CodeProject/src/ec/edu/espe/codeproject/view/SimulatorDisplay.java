a345c64c1b5e9127df28588a99527a4c38b13d70
DanielaLTM2206 <danielatituaa@gmail.com>
2022-06-02 01:44:31 -0500

    Modified code

--- 06-Code/CodeProject/src/ec/edu/espe/codeproject/view/SimulatorDisplay.java
+++ 06-Code/CodeProject/src/ec/edu/espe/codeproject/view/SimulatorDisplay.java
@@ -1,5 +1,4 @@
-
-package ec.edu.espe.codeproject.view;
+ package ec.edu.espe.codeproject.view;
 
 /**
  *
@@ -8,5 +7,28 @@ package ec.edu.espe.codeproject.view;
 public class SimulatorDisplay {
     public static void main(String[] args) {
         System.out.println("Viveres Pedro System, MyWayCode");
+        
+         int limitMenu;
+        limitMenu = 1;
+        
+        System.out.println("1. Sale Register ");
+        System.out.println("2. Inventory Record");
+        System.out.println("3. Add New Product");
+        System.out.println("4. Seach Product");
+        System.out.println("5. Invoice History");
+        
+        
+        switch(limitMenu){
+            case 1 -> System.out.println("Record of sales of the day with date and time");
+            case 2 -> System.out.println("Registers the entry and exit of products from inventory");
+            case 3 -> System.out.println("New products are added with name, batch and unit price");
+            case 4 -> System.out.println("Search for a product by name and batch");
+            case 5 -> System.out.println("There will be a record of the invoices issued daily");
+            
+        }
     }
 }
+        
+    
+        
+
